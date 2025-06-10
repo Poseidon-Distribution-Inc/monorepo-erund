9815ddf69de7e51c353a9184d4fbde1da0699572
jay descalsota <jaydescalsota@gmail.com>
2025-06-10 14:29:19 +0800

    build: update dist

--- dist/types/googleDrive.d.ts
+++ dist/types/googleDrive.d.ts
@@ -4,10 +4,23 @@ interface IGoogleDriveErrorResponse {
 interface IGoogleDriveData {
     fileId: string;
     publicLink: string;
+    fileName?: string;
 }
-export interface IGoogleDriveSuccessResponse {
+interface IGoogleDriveFilesData {
+    files: {
+        fileId: string;
+        publicLink: string;
+        fileName: string;
+    }[];
+}
+interface IGoogleDriveSingleSuccessResponse {
     message: string;
     data: IGoogleDriveData;
 }
+interface IGoogleDriveMultipleSuccessResponse {
+    message: string;
+    data: IGoogleDriveFilesData;
+}
+export type IGoogleDriveSuccessResponse = IGoogleDriveSingleSuccessResponse | IGoogleDriveMultipleSuccessResponse;
 export type IGoogleDriveResponse = IGoogleDriveErrorResponse | IGoogleDriveSuccessResponse;
 export {};
--- src/types/googleDrive.ts
+++ src/types/googleDrive.ts
@@ -2,16 +2,36 @@ interface IGoogleDriveErrorResponse {
   error: string;
 }
 
+// Single file data
 interface IGoogleDriveData {
   fileId: string;
   publicLink: string;
+  fileName?: string;
 }
 
-export interface IGoogleDriveSuccessResponse {
+// Multiple file data
+interface IGoogleDriveFilesData {
+  files: {
+    fileId: string;
+    publicLink: string;
+    fileName: string;
+  }[];
+}
+
+// Single file success response
+interface IGoogleDriveSingleSuccessResponse {
   message: string;
   data: IGoogleDriveData;
 }
 
-export type IGoogleDriveResponse =
-  | IGoogleDriveErrorResponse
-  | IGoogleDriveSuccessResponse;
+// Multiple files success response
+interface IGoogleDriveMultipleSuccessResponse {
+  message: string;
+  data: IGoogleDriveFilesData;
+}
+
+// Combined success response type
+export type IGoogleDriveSuccessResponse = IGoogleDriveSingleSuccessResponse | IGoogleDriveMultipleSuccessResponse;
+
+// General response type that can be either error or success (single/multiple)
+export type IGoogleDriveResponse = IGoogleDriveErrorResponse | IGoogleDriveSuccessResponse;
