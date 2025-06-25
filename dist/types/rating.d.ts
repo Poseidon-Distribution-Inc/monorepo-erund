855826b7805cc6d05f86ecf550915f1678834c74
lipey <lyf.cabz@gmail.com>
2025-06-25 17:08:33 +0800

    updated rating

--- dist/types/rating.d.ts
+++ dist/types/rating.d.ts
@@ -5,6 +5,7 @@ export interface IRatingSchema {
     targetType: string;
     score: number;
     comment?: string;
+    reviewer?: IReviewer;
     isActive: boolean;
 }
 export interface IRating extends IRatingSchema {
@@ -12,10 +13,24 @@ export interface IRating extends IRatingSchema {
     createdAt: Date;
     updatedAt: Date;
 }
+interface IReviewer {
+    firstName: string;
+    lastName: string;
+    photoUrl: string;
+}
+interface IUserProfile {
+    firstName?: string;
+    lastName?: string;
+    photoUrl?: string | null;
+    totalTasks?: number;
+    totalClients?: number;
+}
 export interface IRatingResponse {
     averageRating?: number;
     totalRatings?: number;
-    ratings: IRating[];
+    ratingCounts?: Record<number, number>;
+    userProfile?: IUserProfile;
+    ratings?: IRating[];
 }
 interface ISingleRatingResponse {
     rating: IRating;
