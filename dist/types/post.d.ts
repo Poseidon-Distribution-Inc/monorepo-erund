04438dcdc06880c60e469bdcb94c4c391526beee
Chunchunmaru <janjanster13th@gmail.com>
2025-09-23 13:21:12 +0800

    updated user and post types added: advocate role and runnerCurrentLocation rspectively

--- dist/types/post.d.ts
+++ dist/types/post.d.ts
@@ -1,5 +1,5 @@
-type TransactionStatus = "awaiting_match" | "matched" | "processing" | "processing_payment" | "service_completed" | "service_completed_accepted" | "service_completed_paid" | "transaction_completed" | "failed" | "disputed" | "refunded" | "released" | "resolved_split" | "cancelled";
-type PostType = "bid" | "take";
+type TransactionStatus = 'awaiting_match' | 'matched' | 'processing' | 'processing_payment' | 'service_completed' | 'service_completed_accepted' | 'service_completed_paid' | 'transaction_completed' | 'failed' | 'disputed' | 'refunded' | 'released' | 'resolved_split' | 'cancelled';
+type PostType = 'bid' | 'take';
 export interface IPostSchema {
     transactionId?: string;
     postType: PostType;
@@ -80,6 +80,10 @@ export interface IPostSchema {
     referenceNum?: string;
     isActive: boolean;
     progress?: 'in_transit' | 'arrived' | 'picked_up' | 'delivery_in_progress' | 'delivered' | null;
+    runnerCurrentlocation?: {
+        type: string;
+        coordinates: [number, number];
+    };
 }
 export interface IPost extends IPostSchema {
     id: string;
