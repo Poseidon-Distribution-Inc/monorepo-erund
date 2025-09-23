aee36523f339dc8f5cb5d325f6692bb1b1e62f7b
Josh-Banggud <josheabanggud@gmail.com>
2025-09-23 09:50:38 +0800

    build: update dist

--- src/types/post.ts
+++ src/types/post.ts
@@ -101,6 +101,7 @@ export interface IPostSchema {
     orderNum?: string;
     referenceNum?: string;
     isActive: boolean;
+    deliveryProgress?: 'pickup_in_progress' | 'picked_up' | 'delivery_in_progress' | 'delivered' | null;
 }
 export interface IPost extends IPostSchema {
     id: string;
