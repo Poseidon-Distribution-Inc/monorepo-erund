702a1fcd1e6d61d4aa403c13c859dc42bc05ea8e
Chunchunmaru <janjanster13th@gmail.com>
2026-01-05 13:38:29 +0800

    broke down the long string of generak location for usability

--- src/types/user.ts
+++ src/types/user.ts
@@ -17,7 +17,24 @@ export interface ILocalUserSchema extends IBaseUserSchema {
     postalCode?: string;
     countryCode?: string;
     photoUrl?: string;
-    generalLocation: string;
+    generalLocation?:
+        | string
+        | {
+              street?: string;
+              streetNumber?: string;
+              city?: string;
+              district?: string;
+              region?: string;
+              subregion?: string;
+              country?: string;
+              isoCountryCode?: string;
+              postalCode?: string;
+              formattedAddress?: string;
+              name?: string;
+              timezone?: string | null;
+              lat?: number;
+              long?: number;
+          };
     payoutAccountName?: string;
     payoutAccountNumber?: string;
     payoutAccountCode?: string;
